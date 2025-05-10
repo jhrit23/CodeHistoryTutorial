fcfbcf2ad7519b9bc470078a28bfd864d3f309f9
Jonathan Hesler <jeh8443@rit.edu>
2025-05-09 22:51:34 -0400

    Initial Commit

---
+++ CodeHistoryTutorial/CodeHistoryTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryTutorialApp.swift
+//  CodeHistoryTutorial
+//
+//  Created by Jonathan Hesler on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
